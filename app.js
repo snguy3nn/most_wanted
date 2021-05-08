37736ce75a165bdf28d46398f78e0a2b6a1b8cb7
Stephen Nguyen <snguy3nn@gmail.com>
2021-05-08 12:08:37 -0500

    created a function to filter first or last name

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
 'use strict';
 
 function searchByName(){
-    // Grabbing the values from our nameForm form and inputs.
+  
     let firstNameInput = document.forms['nameForm']['fname'].value;
     let lastNameInput = document.forms['nameForm']['lname'].value;
-
-    // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+        if(person.firstName === firstNameInput || person.lastName === lastNameInput){
             return true;
         }
         return false;
     });
     
-    // Rather than console logging, you need to append the filteredPeople to a table.
     if(filteredPeople.length > 0){
-        console.log(filteredPeople);
-    }else{
+        filteredSearch(filteredPeople);
+    }
+    
+    else{
         alert('Sorry, looks like there is no one with that name.');
+    }
+    
+}
+
+function searchByGender(){
+  
+    let gender = document.forms['genderForm']['gender'].value;
+    let filteredPeople = people.filter(function (person) {
+        if(person.gender === gender){
+            return true;
+        }
+        return false;
+    });
+    
+    if(filteredPeople.length > 0){
+        filteredSearch(filteredPeople);
+    }
+
+    else{
+        alert('Please search for male or female');
     }
 }
 
 
+
+function filteredSearch(foundPeople){
+    let concat = ''
+    foundPeople.map(function(el){
+        return concat += `<tr>
+        <td>${el.id}</td>
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td>${el.gender}</td>
+        <td>${el.dob}</td>
+        <td>${el.height}</td>
+        <td>${el.weight}</td>
+        <td>${el.eyeColor}</td>
+        <td>${el.occupation}</td>
+        <td>${el.parents}</td>
+        <td>${el.currentSpouse}</td>
+        </tr>`
+    })      
+    document.getElementById("people").innerHTML = concat
+}
 
 
 
@@ -40,3 +79,5 @@ people.map(function(el){
     <td>${el.currentSpouse}</td>
     </tr>`
 })       
+
+
