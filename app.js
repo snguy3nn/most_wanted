23a964ab8d5e58abdafc45028e19067f4b2cef76
coreyjordanphx <corey.d.jordan66@gmail.com>
2021-05-11 09:09:40 -0500

    committing new functions

--- app.js
+++ app.js
@@ -1,167 +1,160 @@
 'use strict';
 
-function searchByName(){
-  
+function searchByID(filterPeople){
+    let idInput = document.forms['dataForm']['fname'].value;
+    let filteredPeople = filterPeople.filter(function (person) {
+        if(person.id === idInput ){
+            return true;
+        }
+        return false;
+    })
+    return filteredPeople;
+}
+function searchByName(filterPeople){
     let firstNameInput = document.forms['dataForm']['fname'].value;
     let lastNameInput = document.forms['dataForm']['lname'].value;
-    let filteredPeople = people.filter(function (person) {
+    let filteredPeople = filterPeople.filter(function (person) {
         if(person.firstName === firstNameInput || person.lastName === lastNameInput){
             return true;
         }
         return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
-    }
-    
-    else{
-        alert('Sorry, looks like is no one with that information.');
-    }
-    
+    })
+    return filteredPeople;
+   
 }
-
-function searchByGender(){
-  
+function searchByGender(filterPeople){
     let gender = document.forms['dataForm']['gender'].value;
-    let filteredPeople = people.filter(function (person) {
+    let filteredPeople = filterPeople.filter(function (person) {
         if(person.gender === gender){
             return true;
         }
         return false;
-    });
+    })
+    return filteredPeople;
     
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
-    }
 }
 
-function searchByDob(){
-  
-    let dob = document.forms['dataForm']['dob'].value;
-    let filteredPeople = people.filter(function (person) {
-        if(person.dob === dob){
-            return true;
-        }
-        return false;
-    });
+function searchByDOB(filterPeople){
+        let dob = document.forms['dataForm']['dob'].value;
+        let filteredPeople = filterPeople.filter(function (person) {
+            if(person.dob === dob){
+                return true;
+            }
+            return false;
+    })
+    return filteredPeople;
+        
+}   
+function searchByHeight(filterPeople){
+        let height = document.forms['dataForm']['height'].value;
+        let filteredPeople = filterPeople.filter(function (person) {
+            if(person.height == height){
+                return true;
+            }
+            return false;
+    })
+    return filteredPeople;
+}      
+function searchByWeight(filterPeople){
+        let weight = document.forms['dataForm']['weight'].value;
+        let filteredPeople = filterPeople.filter(function (person) {
+            if(person.weight == weight){
+                 return true;
+             }
+            return false;
+    })
+    return filteredPeople;
+}        
     
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
-    }
+function searchByEyeColor(filterPeople){
+        let eyeColor = document.forms['dataForm']['eyeColor'].value;
+        let filteredPeople = filterPeople.filter(function (person) {
+            if(person.eyeColor === eyeColor){
+                return true;
+            }
+            return false;
+    })
+   return filteredPeople;
+} 
+function searchByOccupation(filterPeople){
+        let occupation = document.forms['dataForm']['occupation'].value;
+        let filteredPeople = filterPeople.filter(function (person) {
+            if(person.occupation === occupation){
+                return true;
+            }
+            return false;
+    })
+   return filteredPeople;
+}
+function searchByParents(filterPeople){
+        let parents = document.forms['dataForm']['parents'].value;
+        let filteredPeople = filterPeople.filter(function (person) {
+            if(person.arents === parents){
+                return true;
+            }
+            return false;
+    })
+    return filteredPeople;
+}
+     
+function searchByCurrentSpouse(filterPeople){
+        let currentSpouse = document.forms['dataForm']['currentSpouse'].value;
+        let filteredPeople = filterPeople.filter(function (person) {
+            if(person.currentSpouse === currentSpouse){
+                return true;
+            }
+            return false;
+    })
+    return filteredPeople;
 }
 
-function searchByHeight(){
-  
-    let height = document.forms['dataForm']['height'].value;
-    let filteredPeople = people.filter(function (person) {
-        if(person.height == height){
-            return true;
-        }
-        return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
+function searchByMultiple(){
+    let filteredPeople = people;
+    if(document.forms['dataForm']['currentSpouse'].value != ""){
+        filteredPeople = searchByID(filteredPeople);
     }
-}
-
-function searchByWeight(){
-  
-    let weight = document.forms['dataForm']['weight'].value;
-    let filteredPeople = people.filter(function (person) {
-        if(person.weight == weight){
-            return true;
-        }
-        return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
+    if(document.forms['dataForm']['parents'].value != ''){
+        filteredPeople = searchByParents(filteredPeople);
     }
-}
-
-function searchByEyeColor(){
-  
-    let eyeColor = document.forms['dataForm']['eyeColor'].value;
-    let filteredPeople = people.filter(function (person) {
-        if(person.eyeColor === eyeColor){
-            return true;
-        }
-        return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
+    if(document.forms['dataForm']['occupation'].value != ""){
+        filteredPeople = searchByOccupation(filteredPeople);
     }
-}
-
-function searchByOccupation(){
-  
-    let occupation = document.forms['dataForm']['occupation'].value;
-    let filteredPeople = people.filter(function (person) {
-        if(person.occupation === occupation){
-            return true;
-        }
-        return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
+    if(document.forms['dataForm']['eyeColor'].value != ""){
+        filteredPeople = searchByEyeColor(filteredPeople);
     }
-}
-
-function searchByParents(){
-  
-    let parents = document.forms['dataForm']['parents'].value;
-    let filteredPeople = people.filter(function (person) {
-        if(person.parents === parents){
-            return true;
-        }
-        return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
+    if(document.forms['dataForm']['weight'].value != ''){
+        filteredPeople = searchByWeight(filteredPeople);
     }
-}
-
-function searchByCurrentSpouse(){
-  
-    let currentSpouse = document.forms['dataForm']['currentSpouse'].value;
-    let filteredPeople = people.filter(function (person) {
-        if(person.currentSpouse === currentSpouse){
-            return true;
-        }
-        return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        filteredSearch(filteredPeople);
+    if(document.forms['dataForm']['height'].value != ''){
+        filteredPeople = searchByHeight(filteredPeople);
     }
-}
-
+    if(document.forms['dataForm']['dob'].value != ''){
+        filteredPeople = searchByDOB(filteredPeople);
+    }
+    if(document.forms['dataForm']['gender'].value != ''){
+        filteredPeople = searchByGender(filteredPeople);
+    }
+       
 function filteredSearch(foundPeople){
-    let concat = ''
+    let concat = ""
     foundPeople.map(function(el){
         return concat += `<tr>
-        <td>${el.id}</td>
-        <td>${el.firstName}</td>
-        <td>${el.lastName}</td>
-        <td>${el.gender}</td>
-        <td>${el.dob}</td>
-        <td>${el.height}</td>
-        <td>${el.weight}</td>
-        <td>${el.eyeColor}</td>
-        <td>${el.occupation}</td>
-        <td>${el.parents}</td>
-        <td>${el.currentSpouse}</td>
-        </tr>`
-    })      
-    document.getElementById("people").innerHTML = concat
+    <td>${el.id}</td>
+    <td>${el.firstName}</td>
+    <td>${el.lastName}</td>
+    <td>${el.gender}</td>
+    <td>${el.dob}</td>
+    <td>${el.height}</td>
+    <td>${el.weight}</td>
+    <td>${el.eyeColor}</td>
+    <td>${el.occupation}</td>
+    <td>${el.parents}</td>
+    <td>${el.currentSpouse}</td>
+    </tr>`
+  
+})
+document.getElementById("people").innerHTML = concat
 }
-
-
 
 people.map(function(el){
     document.getElementById("people").innerHTML += `<tr>
@@ -177,6 +170,5 @@ people.map(function(el){
     <td>${el.parents}</td>
     <td>${el.currentSpouse}</td>
     </tr>`
-})       
-
+})
 
